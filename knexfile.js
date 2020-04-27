d7238ab57aac43aeddb0f0a1399b7d92315329ec
wildcard329 <enkiduwildman@gmail.com>
2020-04-27 12:12:50 -0700

    new commit

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     connection: {
       host: '127.0.0.1',
       user: 'postgres',
-      password: 'aLgrec82#gajik23r',
+      password: '',
       database: 'table'
     },
     migrations: {
